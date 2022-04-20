d17242deba4d3a85a18008a5a781746f0a70b935
Adrici <cirellia5@gmail.com>
2022-04-20 19:41:54 -0300

    CategoryResource

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,14 +1,20 @@
 package com.devsuperior.dscatalog.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Category implements Serializable {
     private static final long serialVersionUIDO = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
 
     public Category() {
 
