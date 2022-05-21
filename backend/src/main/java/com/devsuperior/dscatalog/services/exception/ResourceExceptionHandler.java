f09af1e9c14aa81445d442d1c2570bb885e6ba6d
Adrici <cirellia5@gmail.com>
2022-05-21 15:59:08 -0300

    Custom validation response

--- backend/src/main/java/com/devsuperior/dscatalog/services/exception/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/exception/ResourceExceptionHandler.java
@@ -1,12 +1,16 @@
 package com.devsuperior.dscatalog.services.exception;
 
 import com.devsuperior.dscatalog.resources.exceptions.StanderError;
+import com.devsuperior.dscatalog.resources.exceptions.ValidationError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Field;
 import java.time.Instant;
 
 @ControllerAdvice
@@ -35,6 +39,26 @@ public class ResourceExceptionHandler {
         err.setError("Database Execption");
         err.setMessage((e.getMessage()));
         err.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> validation (MethodArgumentNotValidException e, HttpServletRequest request){
+
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY; //422 http
+       //objeto de resposta especial
+        ValidationError err = new ValidationError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Validation Execption");
+        err.setMessage((e.getMessage()));
+        err.setPath(request.getRequestURI());
+
+
+        for (FieldError f : e.getBindingResult().getFieldErrors()){
+            err.addError(f.getField(), f.getDefaultMessage());
+
+        }
 
         return ResponseEntity.status(status).body(err);
     }
